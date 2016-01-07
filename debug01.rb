76ae59723b7960addd7c4bb0b745f14eed80e4d2
Elaya Sison <asison13@gmail.com>
2016-01-06 18:47:44 -0800

    First commit

---
+++ debug01.rb
@@ -0,0 +1,5 @@
+list = [{'yvr' => 'Vancouver'}, {'yba' => 'Banff'}, {'yyz' => 'Toronto'}, {'yxx' => 'Abbotsford'}, {'ybw' => 'Calgary'}]
+
+# Why is it returning nil instead of first element of the list above
+#initial file didn't have square bracket for array, and the elements weren't divided with curly brackets 
+p list[0]
---
+++ debug04.rb
@@ -0,0 +1,21 @@
+def char_count(list)
+  #letters = {}
+list.map{|word| word.length}
+    #word.split('').each { |letter| 
+      # if !letters[letter].has_key? 
+        # letters.lett()
+      # end
+      # letters[letter] += 1 
+    # }
+  #end
+end
+  # letters
+# end
+
+# Why the long face(error)? 
+# 1. This should return count of each letter in the list
+
+puts char_count(['apples', 'oranges', 'hipsters', 'are', 'same'])
+
+
+# 2. What are the improvements you can do to above code?
